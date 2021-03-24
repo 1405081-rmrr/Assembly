3e9f575f04913ca1e9c9c7fd5b936930df34beaa
Roktim Reza <1405081-rmrr@users.noreply.github.com>
2021-03-24 12:36:51 +0600

    2 ta input neya jay.output ashe

--- 4th Offline/inputtake.asm
+++ 4th Offline/inputtake.asm
@@ -2,9 +2,11 @@ INCLUDE 'EMU8086.INC'      ;include an assembly library
 .MODEL SMALL
 .STACK 100h
 .DATA        
-    ARR DB 50 DUP(?) ; declare array with null value initially 
+    ARR DB 50 DUP(?)
+    ARR2 DB 50 DUP(?) ; declare array with null value initially 
    ; MSG DW 0AH,0DH, 'ENTER VALUES : $' 
-    MSG DW 0AH,0DH, 'ENTER VALUES : $' 
+    MSG DW 0AH,0DH, 'ENTER first matrix VALUES : $'  
+    MSG2 DW 0AH,0DH, 'ENTER sECOND matrix VALUES : $' 
     P DB 0
 .CODE
     MAIN PROC
@@ -14,20 +16,130 @@ INCLUDE 'EMU8086.INC'      ;include an assembly library
         XOR CX,CX
         MOV SI,0 
         
-        ENTER:
+        ENTER: 
+        ;FIRST
         LEA DX,MSG
         MOV AH,9
         INT 21H
         
+        mov ah,2
+        MOV DL,0DH
+        INT 21H 
+        MOV DL,0AH
+        INT 21H
+        
         MOV AH,1
         INT 21H 
-        CMP AL,0DH
-        JE OUTPUT
         AND AL,0FH ;convert from ascii value to real value
         
         MOV ARR[SI],AL
         INC SI
-        JMP ENTER
+         
+        ;SECOND 
+        MOV AH,2
+        MOV DL,20H
+        INT 21H
+        
+        
+        
+        MOV AH,1
+        INT 21H 
+        
+        AND AL,0FH ;convert from ascii value to real value
+        
+        MOV ARR[SI],AL
+        INC SI
+        
+        ;THIRD
+        mov ah,2
+        MOV DL,0DH
+        INT 21H 
+        MOV DL,0AH
+        INT 21H
+        
+        MOV AH,1
+        INT 21H 
+        
+        AND AL,0FH ;convert from ascii value to real value
+        
+        MOV ARR[SI],AL
+        INC SI
+        
+        ;FOURTH
+        mov ah,2
+        MOV DL,20H
+        INT 21H
+        
+        MOV AH,1
+        INT 21H 
+        
+        AND AL,0FH ;convert from ascii value to real value
+        
+        MOV ARR[SI],AL
+        INC SI
+        
+        
+        ;SECOND MATRIX
+        LEA DX,MSG2
+        MOV AH,9
+        INT 21H 
+        MOV SI,0
+        
+        ;FIRST
+        mov ah,2
+        MOV DL,0DH
+        INT 21H
+        MOV DL,0AH
+        INT 21H
+        
+        MOV AH,1
+        INT 21H 
+        
+        AND AL,0FH ;convert from ascii value to real value
+        
+        MOV ARR[SI],AL
+        INC SI
+       
+        ;SECOND
+        mov ah,2
+        MOV DL,20H
+        INT 21H
+        
+        MOV AH,1
+        INT 21H 
+       
+        AND AL,0FH ;convert from ascii value to real value
+        
+        MOV ARR[SI],AL
+        INC SI
+      
+        ;THIRD
+        mov ah,2
+        MOV DL,0DH
+        INT 21H 
+        MOV DL,0AH
+        INT 21H
+        
+        MOV AH,1
+        INT 21H 
+        
+        AND AL,0FH ;convert from ascii value to real value
+        
+        MOV ARR[SI],AL
+        INC SI
+       
+        ;FOURTH
+        mov ah,2
+        MOV DL,20H
+        INT 21H
+        
+        MOV AH,1
+        INT 21H 
+        
+        AND AL,0FH ;convert from ascii value to real value
+        
+        MOV ARR[SI],AL
+        
         
         OUTPUT:
         MOV SI,0
@@ -79,7 +191,7 @@ INCLUDE 'EMU8086.INC'      ;include an assembly library
         OUTPUT4:
         
         MOV AH,2
-        MOV DL,0AH
+        MOV DL,20H
         INT 21H 
                
         MOV AL,ARR[SI]
